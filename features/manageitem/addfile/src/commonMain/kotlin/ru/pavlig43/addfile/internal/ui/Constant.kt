20cd73ef20b991c19214f8027dc11a6fbb8d9794
user <pavlig43@gmail.com>
2025-09-02 21:08:02 +0300

    удалил лишнее

--- features/manageitem/addfile/src/commonMain/kotlin/ru/pavlig43/addfile/internal/ui/Constant.kt
+++ features/manageitem/addfile/src/commonMain/kotlin/ru/pavlig43/addfile/internal/ui/Constant.kt
@@ -1,14 +1,10 @@
 package ru.pavlig43.addfile.internal.ui
 
-internal const val CHOOSE_FILE = "Выбери файл"
 internal const val IS_UPLOAD = "Загружено"
-internal const val IS_NOT_UPLOAD = "Еще не Загружено"
 internal const val FILES = "Файлы"
 internal const val ADD_FILE = "Добавить файл"
 internal const val REQUIRED_FILE_ADD= "Необходимо добавить файлы"
 internal const val RETRY_LOAD = "Повторить загрузку"
 internal const val REMOVE = "Удалить"
 internal const val FILE_EXISTS_CONFIRMATION_MESSAGE= "Файл с данным именем существует"
-internal const val GO_TO_DIRECTORY = "Перейти в папку"
 internal const val COPY_TEXT = "Копировать текст"
-internal const val ERROR_LOAD_INIT = "Произошла ошибка загрузки документов"
