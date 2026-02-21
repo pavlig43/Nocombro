91a7bada35f37ad3da6dc08f6ed034a22a7c153a
user <pavlig43@gmail.com>
2026-02-21 11:33:27 +0300

    create IngredientUi

---
+++ features/form/transaction/src/commonMain/kotlin/ru/pavlig43/transaction/internal/update/tabs/component/opzs/ingridients/IngredientUi.kt
@@ -0,0 +1,14 @@
+package ru.pavlig43.transaction.internal.update.tabs.component.opzs.ingridients
+
+import ru.pavlig43.tablecore.model.IMultiLineTableUi
+
+data class IngredientUi(
+    override val composeId: Int,
+    val transactionId: Int = 0,
+    val batchId: Int = 0,
+    val movementId: Int = 0,
+    val productId: Int = 0,
+    val productName: String = "",
+    val vendorName: String = "",
+    val count: Int = 0,
+): IMultiLineTableUi
